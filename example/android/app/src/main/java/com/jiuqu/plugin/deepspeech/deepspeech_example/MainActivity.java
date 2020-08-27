403bd2e6200186379143cd2071d5f260e1f17243
孙晓群 <>
2020-08-27 15:03:57 +0800

    first commit

---
+++ example/android/app/src/main/java/com/jiuqu/plugin/deepspeech/deepspeech_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.jiuqu.plugin.deepspeech.deepspeech_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/DeepspeechPlugin.h
@@ -0,0 +1,4 @@
+#import <Flutter/Flutter.h>
+
+@interface DeepspeechPlugin : NSObject<FlutterPlugin>
+@end
